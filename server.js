dc225799e2d8c9fcc773bd50c1f933dbad677bb2
jordanmowry <jordan.mowry@gmail.com>
2024-07-17 14:04:12 -0400

    get rid of comments

--- server.js
+++ server.js
@@ -1,36 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { validateImageUrls } from './middleware/validateImageUrls.js';
-// import { sendFilesAsResponse } from './middleware/sendFilesAsResponse.js';
 import { filterImageFromURL, sendFilesAsResponse } from './util/util.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// Init the Express application
+// Initialize the Express application
 const app = express();
 
 // Set the network port
-const port = process.env.PORT || 8080;
+const { PORT, EB_URL } = process.env;
+const port = PORT || 8080;
+const domain = EB_URL || 'http://localhost:';
 
 // Use the body parser middleware for post requests
 app.use(bodyParser.json());
 
-// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-// GET /filteredimage?image_url={{URL}}
-// endpoint to filter an image from a public url.
-// IT SHOULD
-//    1
-//    1. validate the image_url query
-//    2. call filterImageFromURL(image_url) to filter the image
-//    3. send the resulting file in the response
-//    4. deletes any files on the server on finish of the response
-// QUERY PARAMATERS
-//    image_url: URL of a publicly accessible image
-// RETURNS
-//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-/**************************************************************************** */
+// Endpoint to filter an image from a public URL
 app.get('/filteredimage', validateImageUrls, async (req, res, next) => {
   const imageUrls = req.query.image_url.split(',');
   try {
@@ -43,14 +30,14 @@ app.get('/filteredimage', validateImageUrls, async (req, res, next) => {
     next({ status: 500, message: 'Error processing images' });
   }
 });
-//! END @TODO1
 
 // Root Endpoint
 // Displays a simple message to the user
 app.get('/', async (req, res) => {
-  res.send('try GET /filteredimage?image_url={{}}');
+  res.send('try GET /filteredimage?image_url={{URL}}');
 });
 
+// Error handling middleware
 app.use((error, req, res, next) => {
   const { status = 500, message = 'Something went wrong!' } = error;
   res.status(status).json({ error: message });
@@ -58,6 +45,6 @@ app.use((error, req, res, next) => {
 
 // Start the Server
 app.listen(port, () => {
-  console.log(`server running http://localhost:${port}`);
+  console.log(`server running ${domain}${port}`);
   console.log(`press CTRL+C to stop server`);
 });
